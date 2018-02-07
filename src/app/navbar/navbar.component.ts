37b6feb245083f6f10b398c1b3efef7dea14e660
Ying Liu <yliu12@bsu.edu>
2018-02-07 16:57:06 -0500

    add fontawsome attempt1

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,15 @@
-import {NgModule} from '@angular/compiler/src/core';
-
-import { AppComponent } from '../app.component';
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
-
-
-
