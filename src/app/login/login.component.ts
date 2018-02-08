46b97cc5b927b5885e3cff8a5ea7f3b603c29db4
Ying Liu <yliu12@bsu.edu>
2018-02-08 17:06:37 -0500

    modified log in page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,15 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  username = '';
-  password = '';
-  position = '';
-  loop = '';
-  stop = '';
+  login = {
+    username: '',
+    password: '',
+    position: '',
+    loop: 'Green Loop',
+    stop: 'Scheidler'
+  };
+  loopDdlValues = [
+    {key: 'G', val: 'Green Loop'},
+    {key: 'R', val: 'Red Loop'},
+    {key: 'S', val: 'Sunday Loop'},
+    {key: 'B', val: 'Blue Loop'}
+  ];
 
-  constructor() { }
+  gLoopStops = [
+    'Notrh Shelter - N',
+    'Anthony - N',
+    'Alumni',
+    'Stadium',
+    'Scheidler'];
+
+
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onClickLogin() {
+  }
+
+  onClickLoopOption(option) {
+    this.login.loop = option;
+    console.log(option);
+  }
+
+  onClickStopOption(stop) {
+    this.login.stop = stop;
+    console.log(stop);
+  }
 }
