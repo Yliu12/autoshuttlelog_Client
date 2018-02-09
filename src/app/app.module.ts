bec22f3557597c916b9b82138377a940a1842afb
Ying Liu <yliu12@bsu.edu>
2018-02-09 15:21:58 -0500

    add offline, service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
-
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
-
+import {LoginComponent} from './login/login.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,10 +17,12 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
 
