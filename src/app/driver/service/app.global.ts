580cde34ebf2ba983b88471b6003f09e9b23c546
Yliu12 <yliu12@bsu.edu>
2018-04-05 14:49:33 -0400

    Update app.global.ts

--- src/app/driver/service/app.global.ts
+++ src/app/driver/service/app.global.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppGlobals {
-  readonly baseAppUrl: string = 'http://tbuslog01.aws.bsu.edu:8443';
-  readonly baseAPIUrl: string = 'http://10.196.36.163:8080/shuttlelog';
+  readonly baseAppUrl: string = 'https://tbuslog01.aws.bsu.edu:8443';
+  readonly baseAPIUrl: string = 'https://tbuslog01.aws.bsu.edu:8443/shuttlelog';
 }
