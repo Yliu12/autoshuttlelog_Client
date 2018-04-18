7af459752be2b7212551791ab00943d90a292f28
yliu12 <yliu12@bsu.edu>
2018-04-18 00:46:43 -0400

    edit sw update config, add driver name on screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private swUpdate: SwUpdate) {
+  }
 
+  ngOninit() {
+    this.swUpdate.available.subscribe(event => {
+      console.log('A newer version is now available. Refresh the page now to update the cache');
+    });
+    this.swUpdate.checkForUpdate();
+  }
 }
