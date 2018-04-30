f4b82c2a0cc52cf6f744dae4ca023a57b40aff0c
Ying Liu <yliu12@bsu.edu>
2018-04-30 10:37:34 -0400

    'app.global.ts'

--- src/app/driver/service/app.global.ts
+++ src/app/driver/service/app.global.ts
@@ -5,10 +5,10 @@ export class AppGlobals {
 
   // server settings
   readonly baseAppUrl: string = 'https://tbuslog01.aws.bsu.edu:8443';
-  //  readonly baseAPIUrl: string = 'https://tbuslog01.aws.bsu.edu:8443/shuttlelog';
+  readonly baseAPIUrl: string = 'https://tbuslog01.aws.bsu.edu:8443/shuttlelog';
 
 
   // local settings
   // readonly baseAppUrl: string = 'https://tbuslog01.aws.bsu.edu:8443';
-  readonly baseAPIUrl: string = 'http://localhost:8080/shuttlelog';
+  // readonly baseAPIUrl: string = 'http://localhost:8080/shuttlelog';
 }
